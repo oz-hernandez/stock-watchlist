95422147298f65dadd0a450801c7e406deb26549
Osmani-Josue Hernandez <osmani.j.hernandez@gmail.com>
2025-02-19 15:11:51 -0500

    make a stock timeline for 1yr, 3yr and 5yr data

---
+++ src/Components/Timeline.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default function Timeline({timelineFunc}: {timelineFunc: React.Dispatch<React.SetStateAction<number>>}) {
+    return (
+        <div className="timeline">
+            <input type='radio' id='radio1' name='timeline' onClick={() => {timelineFunc(1)}} />
+            <label htmlFor='radio1'>1yr</label>
+            <input type='radio' id='radio2' name='timeline' onClick={() => {timelineFunc(3)}} />
+            <label htmlFor='radio2'>3yr</label>
+            <input type='radio' id='radio3' name='timeline' onClick={() => {timelineFunc(5)}} />
+            <label htmlFor='radio3'>5yr</label>
+        </div>
+    );
+}
