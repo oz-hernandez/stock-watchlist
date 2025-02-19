3f2750f70e6ebec0009db96cf9e16225fbcf66d9
Osmani-Josue Hernandez <osmani.j.hernandez@gmail.com>
2025-02-19 15:15:03 -0500

    add our stock timeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import { useState } from 'react';
 import Stocks from './Components/Stocks';
+import Timeline from './Components/Timeline';
 import './App.css';
 
 
-
 function App() {
-  return (
+    const [userTimeline, setTimeline] = useState<number>(3);
+    return (
         <div className="main">
               <h3 className="main-header" style={{fontSize:'2.8rem', color:'#FF1d58', fontWeight:'bold', borderBottom:'1px solid #2F3F4D'}}>
                   Company Indexes
@@ -13,14 +14,15 @@ function App() {
 
             <div className="stock-container">
                 <h3 className="header" style={{fontSize:'20px', color:'white', fontWeight:'bold' }}>
-                    IBM Highs
+                    Apple Highs
                 </h3>
+                <Timeline timelineFunc={setTimeline}/>
                 <div className="stocks">
-                    <Stocks />
+                    <Stocks timeline={userTimeline}/>
                 </div>
             </div>
-      </div>
-  );
+        </div>
+    );
 }
 
 export default App;
