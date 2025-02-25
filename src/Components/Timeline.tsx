10be000444a857201ad54a354aeb8048df323681
Osmani-Josue Hernandez <osmani.j.hernandez@gmail.com>
2025-02-25 14:53:45 -0500

    3yr should be default active

--- src/Components/Timeline.tsx
+++ src/Components/Timeline.tsx
@@ -6,7 +6,7 @@ export default function Timeline({timelineFunc}: {timelineFunc: React.Dispatch<R
         <div className="timeline">
             <input type='radio' id='radio1' name='timeline' onClick={() => {timelineFunc(1)}} />
             <label htmlFor='radio1'>1yr</label>
-            <input type='radio' id='radio2' name='timeline' onClick={() => {timelineFunc(3)}} />
+            <input type='radio' id='radio2' name='timeline' defaultChecked  onClick={() => {timelineFunc(3)}} />
             <label htmlFor='radio2'>3yr</label>
             <input type='radio' id='radio3' name='timeline' onClick={() => {timelineFunc(5)}} />
             <label htmlFor='radio3'>5yr</label>
